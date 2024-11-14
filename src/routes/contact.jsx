dfbd3bc05277d0f897fb9c7bb97ab6eef43a811f
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-11-14 11:13:48 +0200

    ...not found data]

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -10,6 +10,12 @@ export async function action({ request, params }) {
 
 export async function loader({ params }) {
     const contact = await getContact(params.contactId);
+    if (!contact) {
+        throw new Response('', {
+            status: 404,
+            statusText: 'Not Found',
+        });
+    }
     return { contact };
 }
 
@@ -80,7 +86,10 @@ export default function Contact() {
 
 function Favorite({ contact }) {
     const fetcher = useFetcher();
-    const favorite = contact.favorite;
+    const favorite = fetcher.formData
+        ? fetcher.formData.get('favorite') === 'true'
+        : contact.favorite;
+
     return (
         <fetcher.Form method='post'>
             <button
